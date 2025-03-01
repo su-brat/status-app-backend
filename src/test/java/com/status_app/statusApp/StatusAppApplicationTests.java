dc8c86b3b5370b4693f2a040831fc8e52ee9e5ff
Swagatika1999 <swagatikamishra@Swagatikas-MacBook-Air.local>
2025-03-01 09:43:25 +0530

    committed refactored changes

---
+++ src/test/java/com/status_app/statusApp/StatusAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.status_app.statusApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StatusAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
