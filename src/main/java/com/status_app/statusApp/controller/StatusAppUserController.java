8c39832c3e383d5779b57cec769366b74dd53b39
Subrat Kumar Pattanaik <37294184+su-brat@users.noreply.github.com>
2025-03-01 13:54:25 +0530

    refactor: changed api base path mapping

--- src/main/java/com/status_app/statusApp/controller/StatusAppUserController.java
+++ src/main/java/com/status_app/statusApp/controller/StatusAppUserController.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 
 @RestController
-@RequestMapping("/api/statusApp")
+@RequestMapping("/api")
 public class StatusAppUserController {
 
     @Autowired
--- src/main/java/com/status_app/statusApp/helper/DtoMapperHelper.java
+++ src/main/java/com/status_app/statusApp/helper/DtoMapperHelper.java
@@ -3,7 +3,6 @@ package com.status_app.statusApp.helper;
 import com.status_app.statusApp.dto.ServiceDTO;
 import com.status_app.statusApp.entity.ServiceEntity;
 import org.mapstruct.Mapper;
-import org.mapstruct.Mapping;
 import org.mapstruct.ReportingPolicy;
 
 import java.util.List;
