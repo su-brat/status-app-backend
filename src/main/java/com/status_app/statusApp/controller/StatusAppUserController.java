37ced3bedcf0d6159a6bb66cabcacab07ee97b23
Swagatika1999 <swagatikamishra@Swagatikas-MacBook-Air.local>
2025-03-04 01:24:03 +0530

    CORS changes

--- src/main/java/com/status_app/statusApp/controller/StatusAppUserController.java
+++ src/main/java/com/status_app/statusApp/controller/StatusAppUserController.java
@@ -17,6 +17,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api")
+@CrossOrigin(origins = {"http://localhost:5173","https://service-status-tracker.netlify.app"})
 public class StatusAppUserController {
 
     @Autowired
